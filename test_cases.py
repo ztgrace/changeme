4e5467e06346f3ecee236c13579d5c6b6f5fbf37
Zach Grace <ztgrace@gmail.com>
2016-03-11 12:00:26 -0600

    Rename and cleanup

--- test_cases.py
+++ test_cases.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-# nosetests --with-coverage --cover-erase --cover-package=scanner
+# nosetests --with-coverage --cover-erase --cover-package=changeme test_cases.py
 
 """
  TODO
     - more tests around scan
 """
 
-import scanner
+import changeme
 from nose.tools import *
 import yaml
 import logging
@@ -20,7 +20,7 @@ tomcat_name = 'Apache Tomcat'
 jboss_name = 'JBoss AS 6'
 
 def get_cred(name):
-    creds = scanner.load_creds()
+    creds = changeme.load_creds()
     for i in creds:
         if i['name'] == name:
             return i
@@ -29,63 +29,63 @@ def get_cred(name):
     is_yaml tests
 """
 def test_is_yaml_true():
-    assert scanner.is_yaml(tomcat_yaml) == True
+    assert changeme.is_yaml(tomcat_yaml) == True
 
 def test_is_yaml_false():
-    assert scanner.is_yaml("/etc/hosts") == False
+    assert changeme.is_yaml("/etc/hosts") == False
 
 """
     parse_yaml tests
 """
 def test_parse_yaml_good():
-    assert scanner.parse_yaml(tomcat_yaml)
+    assert changeme.parse_yaml(tomcat_yaml)
 
 @raises(yaml.scanner.ScannerError)
 def test_parse_yaml_bad():
-    assert scanner.parse_yaml("/etc/hosts")
+    assert changeme.parse_yaml("/etc/hosts")
 
 """
     load_creds
 """
 def test_load_creds_good():
-    scanner.logger = scanner.setup_logging(False, False, None)
-    scanner.load_creds()
+    changeme.logger = changeme.setup_logging(False, False, None)
+    changeme.load_creds()
 
 """
     validate_cred
 """    
 def test_validate_cred():
-    scanner.logger = scanner.setup_logging(False, False, None)
-    creds = scanner.load_creds()
+    changeme.logger = changeme.setup_logging(False, False, None)
+    creds = changeme.load_creds()
     
     cred = creds[random.randrange(0, len(creds))]
     while True:
         key = random.choice(cred.keys())
-        if key in scanner.required_keys:
+        if key in changeme.required_keys:
             cred.pop(key)
             break
     
-    assert scanner.validate_cred(cred, "/dev/null") == False
+    assert changeme.validate_cred(cred, "/dev/null") == False
 
 """
     setup_logging tests
 """
 def test_setup_logging():
-    logger = scanner.setup_logging(False, False, None)
+    logger = changeme.setup_logging(False, False, None)
     assert logger.isEnabledFor(logging.WARNING)
 
 def test_setup_logging_verbose():
-    logger = scanner.setup_logging(True, False, None)
+    logger = changeme.setup_logging(True, False, None)
     assert logger.isEnabledFor(logging.INFO)
 
 def test_setup_logging_debug():
-    logger = scanner.setup_logging(False, True, None)
+    logger = changeme.setup_logging(False, True, None)
     assert logger.isEnabledFor(logging.INFO)
     assert logger.isEnabledFor(logging.DEBUG)
 
 def test_setup_logging_file():
     fh = False
-    logger = scanner.setup_logging(False, False, "/tmp/foo.log")
+    logger = changeme.setup_logging(False, False, "/tmp/foo.log")
     for i in logger.handlers:
         if isinstance(i, logging.FileHandler):
             fh = True
@@ -109,9 +109,9 @@ def test_get_fingerprint_matches_tomcat():
     assert res.status_code == 401
     assert res.headers.get('WWW-Authenticate')
 
-    creds = scanner.load_creds()
-    scanner.logger = scanner.setup_logging(False, False, None)
-    matches = scanner.get_fingerprint_matches(res, creds)
+    creds = changeme.load_creds()
+    changeme.logger = changeme.setup_logging(False, False, None)
+    matches = changeme.get_fingerprint_matches(res, creds)
 
     matched = False
     for i in matches:
@@ -135,9 +135,9 @@ def test_get_fingerprint_matches_jboss():
     assert res.status_code == 200
     assert "Welcome to the JBoss AS 6 Admin Console" in res.text
 
-    creds = scanner.load_creds()
-    scanner.logger = scanner.setup_logging(False, False, None)
-    matches = scanner.get_fingerprint_matches(res, creds)
+    creds = changeme.load_creds()
+    changeme.logger = changeme.setup_logging(False, False, None)
+    matches = changeme.get_fingerprint_matches(res, creds)
 
     matched = False
     for i in matches:
@@ -158,8 +158,8 @@ def test_check_basic_auth_tomcat():
         'adding_headers': { 'Server': 'Apache-Coyote/1.1'}
         })
 
-    scanner.logger = scanner.setup_logging(False, False, None)
-    creds = scanner.load_creds()
+    changeme.logger = changeme.setup_logging(False, False, None)
+    creds = changeme.load_creds()
     cred = None
     for i in creds:
         if i['name'] == tomcat_name:
@@ -167,7 +167,7 @@ def test_check_basic_auth_tomcat():
 
     assert cred['name'] == tomcat_name
 
-    matches = scanner.check_basic_auth("http://127.0.0.1:8080/manager/html", cred, False, False, None)
+    matches = changeme.check_basic_auth("http://127.0.0.1:8080/manager/html", cred, False, False, None)
     assert len(matches) > 0
 
 @responses.activate
@@ -183,8 +183,8 @@ def test_check_basic_auth_tomcat_fail():
     cred = get_cred(tomcat_name)
     assert cred['name'] == tomcat_name
 
-    scanner.logger = scanner.setup_logging(False, False, None)
-    matches = scanner.check_basic_auth("http://127.0.0.1:8080/manager/html", cred, False, False)
+    changeme.logger = changeme.setup_logging(False, False, None)
+    matches = changeme.check_basic_auth("http://127.0.0.1:8080/manager/html", cred, False, False)
     assert len(matches) == 0
 
 """
@@ -203,7 +203,7 @@ def test_check_form_jboss():
     cred = get_cred(jboss_name)
     assert cred['name'] == jboss_name
     
-    matches = scanner.check_form('http://127.0.0.1:8080/admin-console/login.seam', cred, {'JSESSIONID':'foobar'}, 'foobar')
+    matches = changeme.check_form('http://127.0.0.1:8080/admin-console/login.seam', cred, {'JSESSIONID':'foobar'}, 'foobar')
     assert len(matches) > 0
 
 @responses.activate
@@ -218,7 +218,7 @@ def test_check_form_zabbix():
     cred = get_cred('Zabbix')
     assert cred['name'] == 'Zabbix'
     
-    matches = scanner.check_form('http://127.0.0.1/zabbix/index.php', cred, False, False)
+    matches = changeme.check_form('http://127.0.0.1/zabbix/index.php', cred, False, False)
     assert len(matches) > 0
 
 @responses.activate
@@ -233,7 +233,7 @@ def test_check_form_zabbix_fail():
     cred = get_cred('Zabbix')
     assert cred['name'] == 'Zabbix'
     
-    matches = scanner.check_form('http://127.0.0.1/zabbix/index.php', cred, False, False)
+    matches = changeme.check_form('http://127.0.0.1/zabbix/index.php', cred, False, False)
     assert len(matches) == 0
 
 """
@@ -253,7 +253,7 @@ def test_get_csrf_token():
     cred = get_cred(jboss_name)
     assert cred['name'] == jboss_name
 
-    csrf = scanner.get_csrf_token(res, cred)
+    csrf = changeme.get_csrf_token(res, cred)
     assert csrf == 'foobar'
 
 @responses.activate
@@ -270,7 +270,7 @@ def test_get_csrf_token_fail():
     cred = get_cred(jboss_name)
     assert cred['name'] == jboss_name
 
-    csrf = scanner.get_csrf_token(res, cred)
+    csrf = changeme.get_csrf_token(res, cred)
     assert csrf == False
 
 @responses.activate
@@ -286,7 +286,7 @@ def test_get_csrf_token_no_token():
     cred = get_cred('Zabbix')
     assert cred['name'] == 'Zabbix'
 
-    csrf = scanner.get_csrf_token(res, cred)
+    csrf = changeme.get_csrf_token(res, cred)
     assert csrf == False
 
 """
@@ -306,7 +306,7 @@ def test_get_session_id():
     cred = get_cred(jboss_name)
     assert cred['name'] == jboss_name
 
-    sessionid = scanner.get_session_id(res, cred)
+    sessionid = changeme.get_session_id(res, cred)
     assert sessionid['JSESSIONID'] == 'foobar'
 
 @responses.activate
@@ -323,7 +323,7 @@ def test_get_session_id_fail():
     cred = get_cred(jboss_name)
     assert cred['name'] == jboss_name
 
-    sessionid = scanner.get_session_id(res, cred)
+    sessionid = changeme.get_session_id(res, cred)
     assert sessionid == False
 
 @responses.activate
@@ -338,7 +338,7 @@ def test_get_session_id_no_id():
     cred = get_cred(tomcat_name)
     assert cred['name'] == tomcat_name
 
-    sessionid = scanner.get_session_id(res, cred)
+    sessionid = changeme.get_session_id(res, cred)
     assert sessionid == False
 
 @responses.activate
@@ -365,26 +365,26 @@ def test_scan():
 
     threads = 1
     timeout = 5
-    creds = scanner.load_creds()
+    creds = changeme.load_creds()
 
-    scanner.scan(urls, creds, threads, timeout, None)
+    changeme.scan(urls, creds, threads, timeout, None)
 
 @raises(SystemExit)
 def test_dry_run():
     urls = list()
     urls.append("http://127.0.0.1:8080/manager/html")
     urls.append("http://127.0.0.1:8080/admin-console/login.seam")
-    scanner.dry_run(urls)
+    changeme.dry_run(urls)
 
 
 
 def test_build_target_list():
-    scanner.targets = ["127.0.0.1"]
-    creds = scanner.load_creds()
-    urls = scanner.build_target_list(scanner.targets, creds, None, None)
+    changeme.targets = ["127.0.0.1"]
+    creds = changeme.load_creds()
+    urls = changeme.build_target_list(changeme.targets, creds, None, None)
     assert(isinstance(urls, list))
 
-    urls = scanner.build_target_list(scanner.targets, creds, tomcat_name, None)
+    urls = changeme.build_target_list(changeme.targets, creds, tomcat_name, None)
     apache_cred = get_cred(tomcat_name)
     paths = apache_cred['path']
     
@@ -413,9 +413,9 @@ def test_do_scan():
         'body': '<p>Welcome to the JBoss AS 6 Admin Console.</p><input name="javax.faces.ViewState" value="foobar" />',
         'adding_headers': { 'Server': 'Apache-Coyote/1.1', 'Set-Cookie': 'JSESSIONID=foobar'}
         })
-    scanner.creds = scanner.load_creds()
-    scanner.setup_logging(True, True, None)
-    scanner.do_scan("http://127.0.0.1/manager/html", scanner.creds, 10, None)
+    changeme.creds = changeme.load_creds()
+    changeme.setup_logging(True, True, None)
+    changeme.do_scan("http://127.0.0.1/manager/html", changeme.creds, 10, None)
     
 
 
