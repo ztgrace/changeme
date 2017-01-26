5043a61e341fc53dacec09f9a176d224db6168de
AlessandroZ <zanni.alessandro@gmail.com>
2017-01-25 22:02:34 -0600

    Merging AlessandroZ's scanners from PR #18

--- changeme/scanners/ssh.py
+++ changeme/scanners/ssh.py
@@ -1,19 +1,53 @@
 from scanner import Scanner
 import paramiko
+import socket
 
 
-class SSHScanner(Scanner):
+class SSH(Scanner):
 
-    def __init__(self, creds, target, config):
-        super(SSHScanner, self).__init__(creds, target, config)
-        self.success = False
+    def __init__(self, cred, target, config):
+        super(SSH, self).__init__(cred, target, config)
+        self.config = config
+        self.password_found = list()
+        self.target = target
 
     def scan(self):
-        self.config.logger.debug("[%s][scan]" % self._class_name())
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.MissingHostKeyPolicy())
+        self.logger.debug("[%s][scan]" % self._class_name())
+        if self._checkOpenPort(self.target, self.port):
+            for cred in self.creds:
+                if self._check_success(self.target, self.port, cred['username'], cred['password']):
+                    self.password_found.append(
+                        {
+                            'name': self.name,
+                            'username': cred['username'],
+                            'pasword': cred['password'],
+                            'url': '%s:%s' % (self.target, str(self.port))
+                        }
+                    )
+        return self._check_success()
 
+    def check_success(self, hostname, port, user, pwd):
         try:
-            client.connect(self.targets, self.port, )
+            ssh = paramiko.Transport((str(hostname), port))
+            ssh.connect(username=user, password=pwd)
+            ssh.close()
+            self.config.logger.critical('[+] Found %s default cred %s:%s at %s' % (self.name, user, pwd, '%s:%s' % (self.target, str(self.port))))
+            return True
+        except Exception, e:
+            self.config.logger.info('[check_success] Invalid %s default cred %s:%s at %s' % (self.name, user, pwd, '%s:%s' % (self.target, str(self.port))))
+            self.logger.debug('[check_success] error: %s' % str(e))
+            return False
 
-        return self.success
+    def _checkOpenPort(self, ip, port):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(3)
+            result = sock.connect_ex((str(ip), port))
+            sock.shutdown(2)
+            if result == 0:
+                return True
+            else:
+                return False
+        except Exception, e:
+            self.logger.debug('[checkOpenPort] %s' % str(e))
+            return False
