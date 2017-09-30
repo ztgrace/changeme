f079a158d90ce23a20fb55351c2a391e85ef487b
Zach Grace <ztgrace@gmail.com>
2017-09-29 20:39:49 -0500

    Fixing broken tests

---
+++ changeme/tests/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['core', 'http', 'memcached', 'redis_scanner', 'snmp', 'target']
--- changeme/tests/redis_scanner.py
+++ changeme/tests/redis_scanner.py
@@ -16,7 +16,7 @@ redis_args = deepcopy(cli_args)
 redis_args['protocols'] = 'redis'
 redis_args['target'] = '127.0.0.1'
 @mock.patch('argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(**redis_args))
-def test_snmp(mock_args):
+def test_redis(mock_args):
     reset_handlers()
     se = core.main()
     assert se.found_q.qsize() == 1
